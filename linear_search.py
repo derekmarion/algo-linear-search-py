dab7615d84a102d7e5a8bafd660d741e55ec23a7
Derek Marion <derek.j.marion@gmail.com>
2023-10-05 06:56:41 -0700

    First commit for linear_search function that passes all test cases

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,10 @@
-def linear_search(value_to_find, array_to_search_through):
-    # your code here
-    pass
+def linear_search(searchTerm, arr):
+    for element in arr: #iterate through array and compare to search term
+        if element == searchTerm:
+            return arr.index(element)
+    return None
 
-def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+def linear_search_global(searchTerm, arr):
+    output = []
+    i = 0
+    for 
